8b5f9a3439a4393c42638c5b0671aec9315334f3
Sascha Hug <Sascha Hug>
2016-11-23 11:34:34 +0100

    hoffe das klappt

--- Mathinator/app/src/main/java/de/tobiaslamm/httpsmathinator/mathinator/Mathinator1.java
+++ Mathinator/app/src/main/java/de/tobiaslamm/httpsmathinator/mathinator/Mathinator1.java
@@ -2,6 +2,8 @@ package de.tobiaslamm.httpsmathinator.mathinator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Mathinator1 extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class Mathinator1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mathinator1);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_takeapicture);
+            }
+        });
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_showhistory);
+            }
+        });
     }
 }
