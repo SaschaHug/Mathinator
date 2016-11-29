223b271cc44edcb44e66d6d7dfdb876873d246a5
IsThisTheFuture <mail@tobiaslamm.de>
2016-11-29 20:36:58 +0100

    Min-API ist jetzt 21. Material-Theme wird jetzt verwendet. Hauptmenü neu angeordnet. Buttonnames refactored.

--- Mathinator/app/src/main/java/de/dhbw/app/mathinator/CalculatorActivity.java
+++ Mathinator/app/src/main/java/de/dhbw/app/mathinator/CalculatorActivity.java
@@ -1,9 +1,9 @@
 package de.dhbw.app.mathinator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Mathinator/app/src/main/java/de/dhbw/app/mathinator/Mathinator.java
+++ Mathinator/app/src/main/java/de/dhbw/app/mathinator/Mathinator.java
@@ -1,12 +1,12 @@
 package de.dhbw.app.mathinator;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Mathinator extends AppCompatActivity {
+public class Mathinator extends Activity {
 
 
     public Button calcButton;
@@ -16,15 +16,15 @@ public class Mathinator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mathinator);
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button camera_button = (Button) findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 setContentView(R.layout.activity_takeapicture);
             }
         });
 
-        final Button button2 = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button history_button = (Button) findViewById(R.id.history_button);
+        history_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 setContentView(R.layout.activity_showhistory);
             }
