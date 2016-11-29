ed260c4b9c63e352f1ca3b3c6afa1ba4d6e0998f
IsThisTheFuture <mail@tobiaslamm.de>
2016-11-29 11:41:34 +0100

    Manual Calculation Menü + Button hinzugefügt

--- Mathinator/app/src/main/java/de/dhbw/app/mathinator/Mathinator.java
+++ Mathinator/app/src/main/java/de/dhbw/app/mathinator/Mathinator.java
@@ -1,11 +1,14 @@
 package de.dhbw.app.mathinator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Mathinator extends AppCompatActivity {
+
+    public Button calcButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,31 @@ public class Mathinator extends AppCompatActivity {
                 setContentView(R.layout.activity_takeapicture);
             }
         });
+
         final Button button2 = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 setContentView(R.layout.activity_showhistory);
             }
         });
+
+
+
+        showCalculator();
+
+
+    }
+
+    public void showCalculator()
+    {
+        calcButton = (Button) findViewById(R.id.calculator_button);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent showCalc = new Intent(Mathinator.this, CalculatorActivity.class);
+                startActivity(showCalc);
+            }
+        });
+
     }
 }
+
