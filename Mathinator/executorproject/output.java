5d538035b08f7b10f5efb94f956e55e4ce1da30b
TIMe747 <t.saupp@icloud.com>
2016-12-07 11:30:39 +0100

    Testfiles

---
+++ Mathinator/executorproject/output.java
@@ -0,0 +1,39 @@
+package com.example.android.notepad.test;
+
+import de.dhbw.app.mathinator.Mathinator;
+import com.robotium.solo.*;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class HistoryMenuAssert extends ActivityInstrumentationTestCase2<Mathinator> {
+  	private Solo solo;
+  	
+  	public HistoryMenuAssert() {
+		super(Mathinator.class);
+  	}
+
+  	public void setUp() throws Exception {
+        super.setUp();
+		solo = new Solo(getInstrumentation());
+		getActivity();
+  	}
+  
+   	@Override
+   	public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+  	}
+  
+	public void testRun() {
+        //Wait for activity: 'de.dhbw.app.mathinator.Mathinator'
+		solo.waitForActivity(de.dhbw.app.mathinator.Mathinator.class, 2000);
+        //Set default small timeout to 83045 milliseconds
+		Timeout.setSmallTimeout(83045);
+        //Click on History
+		solo.clickOnView(solo.getView(com.example.android.notepad.R.id.history_button));
+        //Wait for activity: 'de.dhbw.app.mathinator.HistoryActivity'
+		assertTrue("de.dhbw.app.mathinator.HistoryActivity is not found!", solo.waitForActivity(de.dhbw.app.mathinator.HistoryActivity.class));
+        //Assert that: 'Gleichung' is shown
+		assertTrue("'Gleichung' is not shown!", solo.waitForView(solo.getView(com.example.android.notepad.R.id.tvEquation, 5)));
+	}
+}
