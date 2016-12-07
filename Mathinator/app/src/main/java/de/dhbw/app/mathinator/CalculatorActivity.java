ddfc2cf71cf62cccb83f1b9a549ff3ab042715e0
IsThisTheFuture <mail@tobiaslamm.de>
2016-12-07 12:36:04 +0100

    Einträge aus dem ManualCalc Menü werden jetzt in die DB gespeichert und können ausgelesen werden

--- Mathinator/app/src/main/java/de/dhbw/app/mathinator/CalculatorActivity.java
+++ Mathinator/app/src/main/java/de/dhbw/app/mathinator/CalculatorActivity.java
@@ -1,30 +1,56 @@
 package de.dhbw.app.mathinator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import de.dhbw.app.mathinator.database.History;
 import de.dhbw.app.mathinator.database.MathinatorDatabaseHelper;
 
 public class CalculatorActivity extends Activity {
 
+    public Button resultButton;
+    public EditText inputField;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-       // MathinatorDatabaseHelper.getInstance(this);
         setContentView(R.layout.activity_calculator);
+        inputField = (EditText) findViewById(R.id.InputField);
 
-        // Get Singleton Instance
-        MathinatorDatabaseHelper databaseHelper = MathinatorDatabaseHelper.getInstance(this);
+        initOnclickListener();
 
-        // Get Read-Write Access to database
-        SQLiteDatabase db = databaseHelper.getWritableDatabase();
 
-        History newManualCalcEntry = new History();
-        newManualCalcEntry.equation = findViewById((R.id.InputField)).toString();
 
+
+    }
+
+
+
+    public void initOnclickListener()
+    {
+        resultButton = (Button) findViewById(R.id.resultButton);
+        resultButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                History newManualCalcEntry = new History();
+                newManualCalcEntry.equation = inputField.getText().toString();
+                newManualCalcEntry.id = "000";
+
+                Log.i("CalculatorActivity", " Value of InputField: " + inputField.getText().toString() );
+
+
+                MathinatorDatabaseHelper databaseHelper = MathinatorDatabaseHelper.getInstance(CalculatorActivity.this);
+                databaseHelper.addEntry(newManualCalcEntry);
+
+            }
+        });
     }
 
 
