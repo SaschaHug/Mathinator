8c27262a1dd069320bfb302cf19e5add545fd292
IsThisTheFuture <mail@tobiaslamm.de>
2017-05-30 09:25:01 +0200

    MathPix API Klasse mit Beispielcode hinzugefügt, OKhttp in gradle.build hinzugefügt

---
+++ Mathinator/app/src/main/java/de/dhbw/app/mathinator/ocr/MathPixAPIHandler.java
@@ -0,0 +1,37 @@
+package de.dhbw.app.mathinator.ocr;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MathPixAPIHandler {
+    /**
+     * Ruft die MathPix API über OKhttp mit dem aufgenommenen Bild auf und liefert das Ergebnis als LaTeX-String zurück
+     */
+
+    private Response response;
+    public Response processSingleImage(){
+        try{
+            OkHttpClient client = new OkHttpClient();
+
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, "{ \"url\" : \"data:image/jpeg;base64,{BASE64-STRING}\" }");
+            Request request = new Request.Builder()
+                    .url("https://api.mathpix.com/v3/latex")
+                    .addHeader("content-type", "application/json")
+                    .addHeader("app_id", "mathpix")
+                    .addHeader("app_key", "34f1a4cea0eaca8540c95908b4dc84ab")
+                    .post(body)
+                    .build();
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            System.err.println("Fehler in processSingleImage()");
+        }
+        return response;
+    }
+
+}
