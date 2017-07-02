a4a4922b9037491fb75df88f59203d97eb7d364f
Sascha Hug <sascha.hug@gmx.de>
2017-07-02 19:14:29 +0200

    Unittests added

---
+++ Unittests/CalcUnitTest2.java
@@ -0,0 +1,42 @@
+package de.dhbw.app.mathinator;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Test;
+
+import de.dhbw.app.mathinator.calculator.CalculatorBaseVisitorImpl;
+import de.dhbw.app.mathinator.calculator.CalculatorLexer;
+import de.dhbw.app.mathinator.calculator.CalculatorParser;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by TSDHBW on 02.07.2017.
+ */
+
+
+
+    public class CalcUnitTest2 {
+        @Test
+        public void checkResult()throws Exception {
+
+        /*Testgleichung um Vorrangregel zu überprüfen*/
+            String equationInput = "10-2*3/3";
+
+            ANTLRInputStream input = null;
+            input = new ANTLRInputStream((equationInput.toString()));
+
+            Double expected = 8.0;
+            CalculatorLexer lexer = new CalculatorLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            CalculatorParser parser = new CalculatorParser(tokens);
+            ParseTree tree = parser.input();
+            CalculatorBaseVisitorImpl calcVisitor = new CalculatorBaseVisitorImpl();
+            Double result = calcVisitor.visit(tree);
+
+            assertEquals(expected,result);
+        }
+    }
+
+
